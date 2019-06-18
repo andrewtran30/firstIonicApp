65f6e228291026a35278461989902eb727f474da
Andrew Tran <andrewtran@moduss-MacBook-Pro-2.local>
2019-06-18 17:04:57 -0400

    First commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+helloWorld() {
+  alert('Modus Application has an error!!!!')
+  console.log('Hello World')
+}
   constructor() {}
 
 }
